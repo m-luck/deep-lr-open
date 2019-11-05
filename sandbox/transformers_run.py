56bb62e4aedbbd31c70ce69746cf1042ff235cc1
unknown <mll469@nyu.edu>
2019-11-05 16:18:35 -0500

    edited comments

--- sandbox/transformers_run.py
+++ sandbox/transformers_run.py
@@ -1,7 +1,7 @@
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
-# OPTIONAL: if you want to have more information on what's happening, activate the logger as follows
+# OPTIONAL: if we want to have more information on what's happening, activate the logger as follows
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -18,10 +18,10 @@ tokens_tensor = torch.tensor([indexed_tokens])
 model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 # Set the model in evaluation mode to deactivate the DropOut modules
-# This is IMPORTANT to have reproducible results during evaluation!
+# This is important to have reproducible results during evaluation
 model.eval()
 
-# If you have a GPU, put everything on cuda
+# CUDA options
 # tokens_tensor = tokens_tensor.to('cuda')
 # model.to('cuda')
 
@@ -30,8 +30,7 @@ with torch.no_grad():
     outputs = model(tokens_tensor)
     predictions = outputs[0]
 
-# get the predicted next sub-word (in our case, the word 'man')
+# get the predicted next sub-word 
 predicted_index = torch.argmax(predictions[0, -1, :]).item()
 predicted_text = tokenizer.decode(indexed_tokens + [predicted_index])
 print(predicted_text)
-# assert predicted_text == 'Who was Jim Henson? Jim Henson was a man'
