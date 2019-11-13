0c631e7a6e30ab7c044c34a07c757ab85b8ef24d
Michael <michael.j.lukiman@gmail.com>
2019-11-12 22:15:54 -0500

    top k

--- sandbox/transformers_run.py
+++ sandbox/transformers_run.py
@@ -1,38 +1,49 @@
 import torch
+
+import heapq
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
 # OPTIONAL: if we want to have more information on what's happening, activate the logger as follows
 import logging
-logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.INFO)
 
 # Load pre-trained model tokenizer (vocabulary)
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 
 # Encode a text inputs
-text = "Who was Rob Fergus ? Bob was a"
-indexed_tokens = tokenizer.encode(text)
+text = "The world is"
 
-# Convert indexed tokens in a PyTorch tensor
-tokens_tensor = torch.tensor([indexed_tokens])
- 
-model = GPT2LMHeadModel.from_pretrained('gpt2')
+for i in range(0,10):
+    indexed_tokens = tokenizer.encode(text)
 
-# Set the model in evaluation mode to deactivate the DropOut modules
-# This is important to have reproducible results during evaluation
-model.eval()
+    # Convert indexed tokens in a PyTorch tensor
+    tokens_tensor = torch.tensor([indexed_tokens])
+     
+    model = GPT2LMHeadModel.from_pretrained('gpt2')
 
-# CUDA options
-# tokens_tensor = tokens_tensor.to('cuda')
-# model.to('cuda')
+    # Set the model in evaluation mode to deactivate the DropOut modules
+    # This is important to have reproducible results during evaluation
+    model.eval()
 
-# Predict all tokens
-with torch.no_grad():
-    outputs = model(tokens_tensor)
-    predictions = outputs[0]
+    # CUDA options
+    # tokens_tensor = tokens_tensor.to('cuda')
+    # model.to('cuda')
 
-# get the predicted next sub-word 
-for index in range(0,10):
-    predicted_index = torch.argmax(predictions[0, -index, :]).item()
-    predicted_text = tokenizer.decode(indexed_tokens + [predicted_index])
+    # Predict all tokens
+    with torch.no_grad():
+        outputs = model(tokens_tensor)
+        predictions = outputs[0]
+
+    val_ind_tuples = [(-pred.item(), i) for i, pred in enumerate(predictions[0,-1,:])]
+
+    heapq.heapify(val_ind_tuples)
+
+    # get the predicted next sub-word 
+    for index in range(0,1):
+        predicted_index = heapq.heappop(val_ind_tuples)[1]
+        predicted_text = tokenizer.decode(indexed_tokens + [predicted_index])
+    text = ' '.join(predicted_text.strip('.,').split(' ')[1:])
     print(predicted_text)
+
+print(predicted_text, "[ Token",predicted_index,"]")
 
