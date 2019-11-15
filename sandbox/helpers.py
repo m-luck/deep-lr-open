718b2a1b370b9d001d080e67ef8e4063910cb458
unknown <michael.j.lukiman@gmail.com>
2019-11-14 19:13:02 -0500

    add align to frame

--- sandbox/helpers.py
+++ sandbox/helpers.py
@@ -1,7 +1,8 @@
+import sys
 
-def map_word_alignment_to_frame(alignment_file: str):
+def map_word_alignment_to_frame(alignment_file: str, frames=(0,74)):
     """
-    Takes an alignment file and returns a list of 3-tuples with word, start_frame of the word, and end_frame of the word
+    Takes an alignment file and returns a list of 3-tuples with word, start_frame of the word, and end_frame of the word.
     
     Params:
         alignment_file - relative path to the alignment text file
@@ -20,14 +21,13 @@ def map_word_alignment_to_frame(alignment_file: str):
     time_start = slices[0][0]
     time_end = slices[-1][1]
 
-    frames = (1, 75)
     word_frames = []
 
     for start, end, word in slices:
 
         start_frame = map_range((time_start, time_end), frames, start) 
         end_frame = map_range((time_start, time_end), frames, end) 
-        word_frames.append(word, start_frame, end_frame)
+        word_frames.append((word, start_frame, end_frame))
 
     return word_frames
 
@@ -39,3 +39,8 @@ def map_range(a, b, t):
     """
     (a_start, a_end), (b_start, b_end) = a, b
     return b_start + ((t - a_start) * (b_end - b_start) / (a_end - a_start))
+
+if __name__ == "__main__":
+    align = sys.argv[1]
+    res = map_word_alignment_to_frame(align)
+    print(res)
