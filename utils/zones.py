01db4ff5d3c8cc950790b4f5481a7bc76bfb38fe
Dan Kramer <kramer425@gmail.com>
2019-11-21 16:27:23 -0500

    work on dataloaders

--- utils/zones.py
+++ utils/zones.py
@@ -21,13 +21,38 @@ def get_grid_align_speaker_dir(base_dir: str, speaker: int) -> str:
     return os.path.join(get_grid_align_dir(base_dir), "s_{}".format(speaker))
 
 
+def get_grid_align_file_path(base_dir: str, speaker: int, video_name: str) -> str:
+    align_speaker_dir = get_grid_align_speaker_dir(base_dir, speaker)
+    return os.path.join(align_speaker_dir, "{}.align".format(video_name))
+
+
 def get_grid_image_base_dir(base_dir: str) -> str:
     return os.path.join(base_dir, "grid", "image")
 
 
 def get_grid_image_speaker_dir(base_dir: str, speaker: int) -> str:
+    """
+    Folder containing all folders of mouth images for a given speaker
+    """
     return os.path.join(get_grid_image_base_dir(base_dir), "s_{}".format(speaker))
+
+
+def get_grid_image_speaker_sentence_dir(base_dir: str, speaker: int, sentence_id: str) -> str:
+    """
+    The folder containing the mouth images of a given sentence (one video)
+    """
+    speaker_dir = get_grid_image_speaker_dir(base_dir, speaker=speaker)
+    return os.path.join(speaker_dir, sentence_id)
 
 
 def get_dlib_face_predictor_path(base_dir: str) -> str:
     return os.path.join(base_dir, "dlib", "shape_predictor_68_face_landmarks.dat")
+
+
+def get_resource_overlapped_file_path(base_dir: str) -> str:
+    resources_dir = os.path.join(base_dir, "grid", "resources")
+    return os.path.join(resources_dir, "list_overlapped.json")
+
+
+def get_cache_dir(base_dir: str) -> str:
+    return os.path.join(base_dir, "grid", "cache")
