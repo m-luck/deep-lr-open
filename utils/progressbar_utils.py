3e032d601eb5ad85206ffc663b5786c967e8b972
Dan Kramer <kramer425@gmail.com>
2019-12-14 09:30:01 -0500

    add logs

--- utils/progressbar_utils.py
+++ utils/progressbar_utils.py
@@ -9,7 +9,8 @@ def get_adaptive_progressbar(max_val: int) -> pb.ProgressBar:
     else:
         ProgressBar = pb.NullBar
 
-    print("max val {}".format(max_val))
+    print("max val {}, type ".format(max_val, type(max_val)))
+
     widgets = [pb.Percentage(), ' ', pb.Bar(), ' ', pb.AdaptiveETA(), ', ', pb.Timer()]
     bar = ProgressBar(max_val, widgets)
     return bar
