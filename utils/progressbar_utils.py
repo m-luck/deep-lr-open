5dd8026fa32f87ad22f12509d7c5319c9430830b
Dan Kramer <kramer425@gmail.com>
2019-12-14 09:25:40 -0500

    add logs

--- utils/progressbar_utils.py
+++ utils/progressbar_utils.py
@@ -7,9 +7,9 @@ def get_adaptive_progressbar(max_val: int) -> pb.ProgressBar:
     if sys.stdout.isatty():
         ProgressBar = pb.ProgressBar
     else:
-        print("loading fake progress bar")
         ProgressBar = pb.NullBar
 
+    print("max val {}".format(max_val))
     widgets = [pb.Percentage(), ' ', pb.Bar(), ' ', pb.AdaptiveETA(), ', ', pb.Timer()]
     bar = ProgressBar(max_val, widgets)
     return bar
