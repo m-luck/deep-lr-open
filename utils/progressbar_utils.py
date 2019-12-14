94d75eb31eaa1dd741a82475ee471d578ecf0353
Dan Kramer <kramer425@gmail.com>
2019-12-14 09:16:49 -0500

    disable progressbar not in terminal

--- utils/progressbar_utils.py
+++ utils/progressbar_utils.py
@@ -1,7 +1,14 @@
+import sys
+
 import progressbar as pb
 
 
 def get_adaptive_progressbar(max_val: int) -> pb.ProgressBar:
+    if sys.stdout.isatty():  # real terminal
+        ProgressBar = pb.ProgressBar
+    else:
+        ProgressBar = pb.NullBar
+
     widgets = [pb.Percentage(), ' ', pb.Bar(), ' ', pb.AdaptiveETA(), ', ', pb.Timer()]
-    bar = pb.ProgressBar(max_val, widgets)
+    bar = ProgressBar(max_val, widgets)
     return bar
